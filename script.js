2cc9e353677e67c5e42a579278a6bde5b7a49501
HeshaniNavodya <heshanidharmarathne51@gmail.com>
2026-02-15 20:19:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+const message = `Hey :)
+
+I just wanted to say...
+
+You randomly make my day better.`;
+
+let typing=false;
+
+/* -------- REAL TIME CLOCK -------- */
+function updateTime(){
+    const now = new Date();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+
+    minutes = minutes < 10 ? "0"+minutes : minutes;
+    hours = hours % 12 || 12;
+
+    document.getElementById("time").innerText = `${hours}:${minutes}`;
+}
+
+setInterval(updateTime,1000);
+updateTime();
+/* --------------------------------- */
+
+
+/* open */
+function openMessage(){
+    document.getElementById("lockscreen").style.display="none";
+    document.getElementById("chat").classList.remove("hidden");
+    typeText();
+}
+
+/* typing */
+function typeText(){
+    const el=document.getElementById("text");
+    let i=0;
+    typing=true;
+    el.innerHTML="";
+
+    function type(){
+        if(i<message.length){
+            el.innerHTML+=message[i]==="\n"?"<br>":message[i];
+            i++;
+            setTimeout(type,35);
+        }else{
+            typing=false;
+            document.getElementById("cursor").style.display="none";
+        }
+    }
+    type();
+}
+
+/* close back */
+function closeMessage(){
+    if(typing) return;
+
+    document.getElementById("chat").classList.add("hidden");
+    document.getElementById("lockscreen").style.display="flex";
+    document.getElementById("cursor").style.display="inline";
+}
